1a92afafd13fa7bdf1038c05e79def4e03aace24
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-06-22 21:04:26 +0800

    refactor: remove unnecessary blank lines in postgreChecker and index files

--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -3,7 +3,6 @@
 require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/utils/envSetter.util.php';
 
-
 $host = $typeConfig['pgHost'];
 $port = $typeConfig['pgPort'];
 $username = $typeConfig['pgUser'];
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require_once  __DIR__ . '/bootstrap.php';
-
 require_once __DIR__ . '/utils/envSetter.util.php';
 
 require_once __DIR__ . '/handlers/mongodbChecker.handler.php';
@@ -10,4 +9,7 @@ require_once __DIR__ . '/handlers/mongodbChecker.handler.php';
 // echo __DIR__ . '/bootstrap.php';
 // echo __DIR__ . '/utils/envSetter.util.php';
 // echo __DIR__ . '/handlers/mongodbChecker.handler.php';
+
+
+
 
