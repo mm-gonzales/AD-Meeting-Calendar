9365910cd229e4ee0b57a5b4f78b0c5564120da1
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-06-24 07:50:35 +0800

    refactor(envSetter): removed unused commented lines

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -24,16 +24,3 @@ $typeConfig = [
     'pgUser' => $_ENV['PG_USER'],
     'pgPass' => $_ENV['PG_PASS'],
 ];
-
-// $typeConfig = [
-//     'host' => $_ENV['PG_HOST'],
-//     'port' => $_ENV['PG_PORT'],
-//     'db'   => $_ENV['PG_DB'],
-//     'user' => $_ENV['PG_USER'],
-//     'pass' => $_ENV['PG_PASS'],
-// ];
-
-// $mongoConfig = [
-//     'uri' => $_ENV['MONGO_URI'],
-//     'db'  => $_ENV['MONGO_DB'],
-// ];
