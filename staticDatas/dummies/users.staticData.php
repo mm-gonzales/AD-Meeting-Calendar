ac440ff7162325ba3ddb812fe1ab6eb89113cf1d
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-07-08 20:33:58 +0800

    refactor: change role value

--- staticDatas/dummies/users.staticData.php
+++ staticDatas/dummies/users.staticData.php
@@ -5,12 +5,12 @@ return[
         'name' => 'john.smith',
         'email' => 'john.smith@mailme.com',
         'password' => 'p@ssW0rd1234',
-        'role' => 'designer'
+        'role' => 'user'
     ],
     [
         'name' => 'Jane Doe',
         'email' => 'jane.doe@example.com',
         'password' => 'MySecurePass2025',
-        'role' => 'developer',
+        'role' => 'admin',
     ],
 ];
