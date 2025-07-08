5ceccca202cc4fa37908f81966fa2ae2da4d013a
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-07-08 21:08:42 +0800

    feat: define COMPONENTS_PATH for component directory

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,7 @@ define('UTILS_PATH', BASE_PATH . '/utils/');
 define('VENDOR_PATH', BASE_PATH . '/vendor/');
 define('HANDLERS_PATH', BASE_PATH . '/handlers/');
 define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
+define('COMPONENTS_PATH', BASE_PATH . '/components/');
 
 chdir(BASE_PATH);
 
